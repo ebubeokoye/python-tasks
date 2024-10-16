6c35c49cddf28802ab97f1ca91da99577a932a50
Chijioke Okwunakwe <15726558+omazim@users.noreply.github.com>
2024-10-16 19:26:44 +0100

    Update serious.py
    Added a little serious comment.

--- serious.py
+++ serious.py
@@ -2,3 +2,6 @@ numbers = [0,1,2,3,4,5,6,7,8,9]
 
 for number in numbers:  
  print (number-1 + number)
+
+# Hey! this is great code!. THe name of the file is serious? Wow. 
+# I'm not the only serious person here. Good to know.
