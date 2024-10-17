a9392562aff3d90a1624f637c271d8afc050cd76
Chijioke Okwunakwe <15726558+omazim@users.noreply.github.com>
2024-10-17 10:31:50 +0100

    Comment on input.py
    Terrifying comment on

--- input.py
+++ input.py
@@ -1,3 +1,5 @@
 import os
 file_details = os.path.splitext((input("filename ")))
 print(file_details[1][1:])
+
+# You're terrifyingly smart!!! I'm watching you closely...so you don't outsmart Aguata people.
